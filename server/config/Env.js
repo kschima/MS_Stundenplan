688de4200cac751aec62c57449fc50cecc64553e
Kevin Schima <kschima@users.noreply.github.com>
2021-07-11 19:26:14 +0200

    Update Env.js

--- server/config/Env.js
+++ server/config/Env.js
@@ -1,6 +1,6 @@
 module.exports = {
-    DB_URL: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : "bolt://0.0.0.0:7687",
-    DB_USER: process.env.NODE_ENV === 'production' ? process.env.DATABASE_USER : "neo4j",
-    DB_PASSWORD: process.env.NODE_ENV === 'production' ? process.env.DATABASE_PASSWORD : "neo4j",
-    DB_NAME: process.env.NODE_ENV === 'production' ? process.env.DATABASE_NAME : ""
+    DB_URL: process.env.DATABASE_URL ? process.env.DATABASE_URL : "bolt://0.0.0.0:7687",
+    DB_USER: process.env.DATABASE_USER ? process.env.DATABASE_USER : "neo4j",
+    DB_PASSWORD: process.env.DATABASE_PASSWORD ? process.env.DATABASE_PASSWORD : "neo4j",
+    DB_NAME: process.env.DATABASE_NAME ? process.env.DATABASE_NAME : ""
 }
