69ba544504364782f9a1705c6afd8354285eebfd
Kevin Schima <kschima@users.noreply.github.com>
2021-07-12 17:36:41 +0200

    Edit rooms api

--- server/routes/rooms-router.js
+++ server/routes/rooms-router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const room_calls = require('../neo4j_calls/room');
+let roomsJson = require('../config/init/rooms.json');
 
 router.get('/', async function (req, res, next) {
-    let result = await room_calls.getAllRooms();
-    console.log("RESULT IS", result);
-    res.status(200).send(result);    
-    return { result };
+    //let result = await room_calls.getAllRooms();
+    console.log("RESULT IS", roomsJson);
+    res.status(200).send(roomsJson);    
+    return { roomsJson };
 })
 
 module.exports = router;
