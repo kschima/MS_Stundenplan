63a62f8700a5861486021c9d1e5264731fd14f1a
Kevin Schima <kschima@users.noreply.github.com>
2021-07-12 10:36:38 +0200

    Add routes to other services

--- client/booking-service/src/App.vue
+++ client/booking-service/src/App.vue
@@ -16,7 +16,7 @@
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
-          :to="item.to"
+          :href="item.to"
           router
           exact
         >
@@ -57,35 +57,31 @@ export default {
       items: [
         {
           title: "Startseite",
-          to: "/"
+          to: "https://sgse2021-ilias.westeurope.cloudapp.azure.com/users/"
         },
         {
-          title: "Mail",
-          to: "/"
+          title: "Nachrichten",
+          to: "https://sgse2021-ilias.westeurope.cloudapp.azure.com/messages/"
         },
         {
           title: "Kurse",
-          to: "/"
+          to: "https://sgse2021-ilias.westeurope.cloudapp.azure.com/courses/"
         },
         {
-          title: "Stundenplan",
-          to: "/"
+          title: "Termine",
+          to: "https://sgse2021-ilias.westeurope.cloudapp.azure.com/appointments/"
         },
         {
           title: "Prüfungen",
-          to: "/"
+          to: "https://sgse2021-ilias.westeurope.cloudapp.azure.com/exams/"
         },
         {
           title: "Raumbuchung",
-          to: "/Bookings"
-        },
-                {
-          title: "Raumplanung",
-          to: "/"
+          to: "https://sgse2021-ilias.westeurope.cloudapp.azure.com/bookings/"
         },
         {
           title: "Ein-/Ausloggen",
-          to: "/"
+          to: "https://sgse2021-ilias.westeurope.cloudapp.azure.com/users/"
         }
       ],
       accountMenuItems: [
