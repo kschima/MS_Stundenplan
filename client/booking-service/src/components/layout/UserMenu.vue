f9becb9b4011ae0800187980eff34aaf0ca715fb
Kevin Schima <kschima@users.noreply.github.com>
2021-07-12 11:41:00 +0200

    Add cookie access

--- client/booking-service/src/components/layout/UserMenu.vue
+++ client/booking-service/src/components/layout/UserMenu.vue
@@ -3,7 +3,9 @@
     <v-menu offset-y>
       <template v-slot:activator="{ on, attrs }">
         <v-icon v-bind="attrs" v-on="on" x-large>mdi-account-circle</v-icon>
+        {{ getUsername() }}
       </template>
+      
       <v-list>
         <v-list-item v-for="(item, index) in items" :key="index">
           <v-list-item-title>{{ item.title }}</v-list-item-title>
@@ -22,6 +24,12 @@ export default {
       { title: "Click Me" },
       { title: "Click Me 2" }
     ]
-  })
+  }),
+  methods: {
+    getUsername() {
+      let currentUser = this.$cookies.get('current-user');
+      return currentUser.firstname + " " + currentUser.lastname;
+    },
+  },
 };
 </script>
